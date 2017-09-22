35d04333a452fd8770c72c8e2b950d507f294bb6
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-09-22 13:50:16 -0700

    Adding the sum function

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function sum(a,b) {
 }
 
 // TODO: Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -31,11 +31,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var product = a * b;
+  return([product,'The product of ' + a + ' and ' + b + ' is ' + product + '.']);
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
